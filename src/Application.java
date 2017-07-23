7a2365cabfaa6a936cd51664e5dff1a7beeb0ea7
Tien Nguyen <tiennv147@gmail.com>
2017-07-23 20:08:42 +0700

    [Services] organize structure of project Create models & services storage

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+    public static void main(String[] args) {
+        //Create MessageStorageService
+
+        //Write program for query & save message here
+
+        //List menu here
+
+        //Using scanner to get action of user
+
+        //Perform action following by input
+    }
+}
--- src/com/chat/models/Message.java
+++ src/com/chat/models/Message.java
@@ -1,4 +1,4 @@
-package com.chat;
+package com.chat.models;
 
 
 public class Message {
